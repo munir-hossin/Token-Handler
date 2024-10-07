e6ccf722e7bbcf43f7dbc69883735447231a4487
munir-hossin <hm.munirul.islam2222@gmail.com>
2024-10-08 01:30:59 +0600

    Token Handler

---
+++ src/sections/Banner.jsx
@@ -0,0 +1,10 @@
+
+
+
+function Banner() {
+    return (
+        <div className="h-96 bg-gray-500 flex justify-center items-center text-white text-5xl font-lato">Please take a token first</div>
+    );
+}
+
+export default Banner;
---
+++ src/sections/FormArea.jsx
@@ -0,0 +1,12 @@
+
+
+
+function FormArea() {
+    return (
+        <div>
+            <h1>This is form area please Start your todays work form here ok</h1>
+        </div>
+    );
+}
+
+export default FormArea;
